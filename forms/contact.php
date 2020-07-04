938a683722fe760d08c06ca5372894e2af5c2bd3
Djamilson <djamilson@gmail.com>
2020-07-04 16:03:20 -0300

    Update teste moveis planejados

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,17 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+
+    'host' => 'ssl://smtp.gmail.com',
+    'port' => '465',
+    'auth' => true,
+    'username' => 'djamilson@gmail.com',
+    'password' => '1costa23',
+   
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
